be50a2a9f335b579aa30067d12fd61db3e6cd1ac
mariakiraga <80244108+mariakiraga@users.noreply.github.com>
2025-10-19 21:28:36 +0200

    Update zad5.sql

--- 02/zad5.sql
+++ 02/zad5.sql
@@ -1,1 +1,30 @@
+INSERT INTO buildings (geometry, name)
+VALUES (
+    ST_GeomFromText('POLYGON((8 4, 10.5 4, 10.5 1.5, 8 1.5, 8 4))'),
+    'Building A'
+);
+
+INSERT INTO buildings (geometry, name)
+VALUES (
+    ST_GeomFromText('POLYGON((4 7, 6 7, 6 5, 4 5, 4 7))'),
+    'Building B'
+);
+
+INSERT INTO buildings (geometry, name)
+VALUES (
+    ST_GeomFromText('POLYGON((3 8, 5 8, 5 6, 3 6, 3 8))'),
+    'Building C'
+);
+
+INSERT INTO buildings (geometry, name)
+VALUES (
+    ST_GeomFromText('POLYGON((9 9, 10 9, 10 8, 9 8, 9 9))'),
+    'Building D'
+);
+
+INSERT INTO buildings (geometry, name)
+VALUES (
+    ST_GeomFromText('POLYGON((1 2, 2 2, 2 1, 1 1, 1 2))'),
+    'Building F'
+);
 
