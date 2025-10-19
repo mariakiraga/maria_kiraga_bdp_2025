0a5335476d62f89adfca2587dfb122562ceef273
mariakiraga <80244108+mariakiraga@users.noreply.github.com>
2025-10-19 21:28:19 +0200

    Update zad4.sql

--- 02/zad4.sql
+++ 02/zad4.sql
@@ -1,1 +1,18 @@
 
+CREATE TABLE buildings (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(POLYGON),
+    name VARCHAR(255)
+);
+
+CREATE TABLE roads (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(LINESTRING),
+    name VARCHAR(255)
+);
+
+CREATE TABLE poi (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(POINT),
+    name VARCHAR(255)
+);
